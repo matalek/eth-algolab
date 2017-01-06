01b91218e507af5fefc028b3674889a6997ff9c0
Aleksander Matusiak <matusiak.aleksander@gmail.com>
2017-01-06 18:22:13 +0100

    Update tester script for nicer executing commands

--- tester.sh
+++ tester.sh
@@ -4,5 +4,5 @@ exec=${1-a.out}
 
 for test in ./tests/*.in; do
     base=${test::${#test}-3}
-    eval "./$exec < $base.in | diff -bsq $base.out -"
+    ./$exec < $base.in | diff -bsq $base.out -
 done
