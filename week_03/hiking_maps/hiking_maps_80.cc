37e4d0e540111f3ce8b1ea990b61e12051cfb61b
Aleksander Matusiak <matusiak.aleksander@gmail.com>
2017-01-24 10:11:41 +0100

    Prettify slower solution for Hiking maps

--- week_03/hiking_maps/hiking_maps_80.cc
+++ week_03/hiking_maps/hiking_maps_80.cc
@@ -11,6 +11,7 @@
 #include <vector>
 #include <set>
 #include <deque>
+#include <climits>
 
 #define REP(i, n) for (int i = 0; i < (n); ++i)
 #define ST first
@@ -31,11 +32,7 @@ typedef K::Segment_2 S;
 typedef K::Point_2 P;
 typedef K::Triangle_2 T;
 
-const int N = 8003;
-P paths[N];
-T maps[N];
-VI under[N];
-
+// Returns whether given point is inside or on boundary of the triangle.
 bool in_triangle(const T& t, const P& p) {
     auto o = t.bounded_side(p);
     return o == CGAL::ON_BOUNDED_SIDE || o == CGAL::ON_BOUNDARY;
@@ -45,39 +42,53 @@ void do_test() {
     int m, n;
     cin >> m >> n;
     int a, b, c, d, e, f;
+
+    vector<P> paths(m);
     REP(i, m) {
         cin >> a >> b;
         paths[i] = P(a, b);
     }
 
+    // Read in maps.
+    vector<T> maps(n);
     REP(i, n) {
+        // Read in points which describe the map.
         P points[6];
         REP(j, 6) {
             cin >> a >> b;
             points[j] = P(a, b);
         }
 
+        // Construct lines correspondint to boundaries of the triangle.
         L edges[3];
         REP(j, 3) { edges[j] = L(points[2 * j], points[2 * j + 1]); }
 
+        // Intersect these lines to create triangle vertices.
         P vertices[3];
         REP(j, 3) {
             auto o = CGAL::intersection(edges[j], edges[(j + 1) % 3]);
             vertices[j] = *boost::get<P>(&*o);
         }
 
+        // Create a triangle from the vertices.
         maps[i] = T(vertices[0], vertices[1], vertices[2]);
     }
 
-    int start = 0, end = 0, cnt = 0, res = -1;
-    int over[m];
-    REP(i, m) { over[i] = 0; }
+    int start = 0, end = 0;  // considered interval
+    int cnt = 0;             // current number of covered path
+    int res = INT_MAX;       // minimal length of interval covering all paths
+    VI over(m);              // how many maps are covering given path
+    vector<VI> under(
+        n);  // for given map keeps vector of paths that this map is covering
 
-    REP(i, n) { under[i].clear(); }
-
+    // Perform sliding window search.
     while (start < n) {
+        // Move end as long as we manage to cover all the paths or reach the
+        // end.
         while (end < n && cnt < m - 1) {
             T map = maps[end];
+
+            // Go over all the paths and check whether this map is covering them.
             bool next = in_triangle(map, paths[0]);
             REP(i, m - 1) {
                 bool tmp = in_triangle(map, paths[i + 1]);
@@ -93,17 +104,19 @@ void do_test() {
             end++;
         }
 
+        // Check if we are covering all the paths.
         if (cnt == m - 1) {
-            if (res == -1 || end - start < res) {
-                res = end - start;
-            }
+            res = min(res, end - start);
         } else {
+            // We have reached the end.
             break;
         }
 
+        // Remove map corresponding to the beggining of the interval.
         for (int path : under[start]) {
             over[path]--;
             if (!over[path]) {
+                // No more maps are covering this path.
                 cnt--;
             }
         }
